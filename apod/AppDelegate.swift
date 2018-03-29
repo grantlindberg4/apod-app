7ca73939b4f803cd1d79dacae51c71ce17932fd9
Lindberg <grantlindberg@grant-lindbergs-imac.local>
2018-03-29 10:57:30 -0700

    Create URL extension to simplify querying

--- apod/AppDelegate.swift
+++ apod/AppDelegate.swift
@@ -9,6 +9,16 @@
 import UIKit
 import Foundation
 
+extension URL {
+    func withQueries(_ queries: [String: String]) -> URL? {
+        var components = URLComponents(url: self, resolvingAgainstBaseURL: true)
+        components?.queryItems = queries.flatMap {
+            URLQueryItem(name: $0.0, value: $0.1)
+        }
+        return components?.url
+    }
+}
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -17,7 +27,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        let url = URL(string: "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")!
+        let baseURL = URL(string: "https://api.nasa.gov/planetary/apod")!
+        let query: [String: String] = [
+            "api_key": "DEMO_KEY",
+            "date": "2011-07-13"
+        ]
+        let url = baseURL.withQueries(query)!
         let task = URLSession.shared.dataTask(with: url) {
             (data, response, error) in
             if let data = data,
