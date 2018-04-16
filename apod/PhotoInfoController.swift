b8d07809e29148ca44e5b56a83cad5234d684024
Lindberg <grantlindberg@Grant-Lindbergs-iMac.local>
2018-04-15 19:30:37 -0700

    Replace DEMO_KEY with personal API key

--- apod/PhotoInfoController.swift
+++ apod/PhotoInfoController.swift
@@ -9,6 +9,8 @@
 import UIKit
 import Foundation
 
+let API_KEY = "8lxcX2I3ErblI9o8cAuYDypoQPRafkLttcBqkO3O"
+
 class PhotoInfoController {
     func fetchPhotoInfo(completion: @escaping (PhotoInfo?) -> Void) {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
@@ -16,12 +18,13 @@ class PhotoInfoController {
         // Bad video date
 //        let currDate = "2018-01-22"
         let query: [String: String] = [
-            "api_key": "DEMO_KEY",
+            "api_key": API_KEY,
             "date": appDelegate.selectedDate!
         ]
         let url = baseURL.withQueries(query)!
         let task = URLSession.shared.dataTask(with: url) {
             (data, response, error) in
+//            print(response!)
             let jsonDecoder = JSONDecoder()
             if let data = data,
                 let photoInfo = try? jsonDecoder.decode(PhotoInfo.self, from: data) {
